6770d7101c527728a3907e9598c08a18678c99fe
StoveC <stove_cjh@126.com>
2020-11-07 16:41:58 +0800

    c

---
+++ git实验/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git实验
+//
+//  Created by Stove on 2020/11/7.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
